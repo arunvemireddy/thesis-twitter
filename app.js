e83614d984cf8cf6315dd40452dc8cd9443dfb63
DongHoon Kim <donghoon5793@gmail.com>
2022-05-09 11:00:33 -0600

    Stardust based : http://localhost:3000/stardust/index.html D3 based : http://localhost:3000/d3/index.html

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ app.use(express.static('graph'));
 
 app.listen(port,()=>{console.log("Server is running"+port)});
 
-app.get('/index',function(req,res){
-    fs.readFile("graph/index.html",(err,data)=>{
+app.get('/',function(req,res){
+    fs.readFile("index.html",(err,data)=>{
     res.writeHead(200,{"Content-Type":"text/html"});
     res.end(data);
     })
